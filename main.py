0c405bc17ee1244d63b8296b21658149a4859e2e
Alpha Jesse <73220143+Mynameisjesse@users.noreply.github.com>
2024-06-06 01:39:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,3 +22,25 @@ class Account:
     def __str__(self):
         return f"Account({self.account_number}, Balance: {self._balance})"
 
+# Person 2
+class SavingsAccount(Account):
+    def __init__(self, account_number, balance=0):
+        super().__init__(account_number, balance)
+        self.interest_rate = 0.005
+        self.withdrawal_limit = 700000
+
+    def deposit(self, amount):
+        if super().deposit(amount):
+            self._balance += amount * self.interest_rate
+            return True
+        return False
+
+    def withdraw(self, amount):
+        if amount <= self.withdrawal_limit:
+            return super().withdraw(amount)
+        return False
+
+    def __str__(self):
+        return f"SavingsAccount({self.account_number}, Balance: {self._balance})"
+
+
